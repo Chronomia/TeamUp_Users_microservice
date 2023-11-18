231fa2c8b92afe91c8da203214f80e53ce28b826
Lawrence Xiaohua Li <ll3598@columbia.edu>
2023-11-18 14:21:22 -0500

    Accidentally pushed secret key anyway, so I deleted and revoked all secret keys that I've pushed :)

--- test/delete_user.py
+++ test/delete_user.py
@@ -2,7 +2,7 @@ import requests
 
 username = "exampleuser"
 
-url = f"http://localhost:8000/users/id/6558405db11eb1c3d4462d0b"
+url = f"http://localhost:8000/users/6558405db11eb1c3d4462d0b"
 
 headers = {
     "Content-Type": "application/json",
