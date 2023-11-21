f2d44bc44d1e008e536a465c6c0c970e70a710c8
Lawrence Xiaohua Li <lawrence.lixiaohua@gmail.com>
2023-11-20 21:07:59 -0500

    Implemented change username functionality

--- service.py
+++ service.py
@@ -294,8 +294,8 @@ async def update_user_profile(user_id: str, user: UpdateUserModel = Body(...)):
     response_model_by_alias=False,
 )
 async def change_username(user_id: str, new_username: UpdateUsername = Body(...)):
-    current_user = mongodb_service["collection"].find_one({"_id": ObjectId(user_id)})
-    if len(current_user) == 1 and str(current_user[0]["_id"]) != user_id:
+    existing_user = list(mongodb_service["collection"].find_one({"username": new_username.username}).limit(1))
+    if len(existing_user) == 1 and str(existing_user[0]["_id"]) != user_id:
         raise HTTPException(status_code=409, detail=f"Username {new_username.username} is already taken")
 
     user = {
