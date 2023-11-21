e8b0b58d8a9229a1839bc91fcdaca85f789318b0
Lawrence Xiaohua Li <ll3598@columbia.edu>
2023-11-21 12:57:41 -0500

    Implement API key for microservice

--- app/api_auth.py
+++ app/api_auth.py
@@ -3,12 +3,12 @@ import os
 from fastapi import HTTPException, Security
 from fastapi.security import api_key
 
-api_key_header = api_key.APIKeyHeader(name="api_key")
+api_key_header = api_key.APIKeyHeader(name="authorization")
 API_KEY = os.environ.get("API_KEY")
 
 
 async def validate_api_key(key: str = Security(api_key_header)):
     if key != API_KEY:
         raise HTTPException(
-            status_code=401, detail="Unauthorized - API Key is wrong"
+            status_code=401, detail="Unauthorized"
         )
